fc7b5748dd0f217e9cad62ae23489d10ddba0491
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-05-05 05:58:15 +0000

    Implement rebase method in Diff

--- src/diff.ts
+++ src/diff.ts
@@ -1,4 +1,4 @@
-import { ConstrainedData } from "./common"
+import { ConstrainedData, Interval } from "./common"
 
 /** A textual edit */
 class Delta extends ConstrainedData {
@@ -16,6 +16,22 @@ class Delta extends ConstrainedData {
      * 
      * @returns whether this object is valid or not
      */
+    public validate(): string | null {
+        if (this.offset >= 0) {
+            return null
+        } else {
+            return `the offset is negative (${this.offset})`
+        }
+    }
+}
+
+/** A conflict occurred when trying to delete the text that the previous diff already deletes */
+class DeleteAlreadyDeletedText extends ConstrainedData {
+    /**
+     * @param offset The offset of the text
+     * @param text The text already deleted
+     */
+    constructor (public offset: number, public text: string) { super() }
     public validate(): string | null {
         if (this.offset >= 0) {
             return null
@@ -46,6 +62,52 @@ class Diff extends ConstrainedData {
         return new Diff(inversed_deltas)
     }
 
+    /**
+     * Re-calculate offsets on top of another base diff (like `git rebase`)
+     *
+     * @param base The new base diff
+     * @returns The new diff that these offsets are calculated on top of `base`
+     */
+    public rebase(base: Diff): Diff | DeleteAlreadyDeletedText {
+        let diff = 0
+        let deltas = []
+        let base_index = 0
+
+        for (const delta of this.deltas) {
+            while (base_index != base.deltas.length && base.deltas[base_index].offset < delta.offset) {
+                const d = base.deltas[base_index]
+                diff += d.insert.length - d.remove.length
+                base_index += 1
+            }
+
+            let i1 = new Interval(-1, 0)
+            if (base_index != 0 && base.deltas.length != 0) {
+                const d = base.deltas[base_index - 1]
+                i1 = new Interval(d.offset, d.remove.length)
+            }
+            let i2 = new Interval(-1, 0)
+            if (base_index < base.deltas.length) {
+                const d = base.deltas[base_index]
+                i2 = new Interval(d.offset, d.remove.length)
+            }
+
+            const i = new Interval(delta.offset, delta.remove.length)
+            if (i.intersect(i1) != null) {
+                // base.deltas[base_index - 1] and delta is overlapped
+                const x = i.intersect(i1)
+                return new DeleteAlreadyDeletedText(x.begin, delta.remove.substr(x.begin - delta.offset, x.length))
+            }
+            if (i.intersect(i2) != null) {
+                // base.deltas[base_index] and delta is overlapped
+                const x = i.intersect(i2)
+                return new DeleteAlreadyDeletedText(x.begin, delta.remove.substr(x.begin - delta.offset, x.length))
+            }
+
+            deltas.push(new Delta(delta.offset + diff, delta.remove, delta.insert))
+        }
+        return new Diff(deltas)
+    }
+
     public validate(): string | null {
         let start = -1
         let end = -1
@@ -67,4 +129,4 @@ class Diff extends ConstrainedData {
     }
 }
 
-export { Delta, Diff }
+export { Delta, Diff, DeleteAlreadyDeletedText }
--- test/test_diff.ts
+++ test/test_diff.ts
@@ -1,8 +1,9 @@
 import * as chai from 'chai'
 const should = chai.should()
 
-import { Delta, Diff } from '../src/diff'
+import { Delta, Diff, DeleteAlreadyDeletedText } from '../src/diff'
 import { ExecutionMode, ConstrainedData } from '../src/common'
+import { fail } from 'assert';
 
 describe('Delta', () => {
     describe('#validate', () => {
@@ -45,11 +46,67 @@ describe('Diff', () => {
             const orig = new Diff([new Delta(0, "x", "xx"), new Delta(1, "yy", "y"), new Delta(3, "z", "z")])
             const inverse = orig.inverse()
 
-            /*
-             * If `orig` is applied to "xyyz", the result is "xxyz".
-             * -> The inverse operation should modify a text "xxyz" to "xyyz".
+            /* Example
+             * - the target text "xyyz"
+             * - `orig` is applied: "xxyz"
+             * - `orig.inverse` is applied to "xxyz": "xyyz"
+             *
+             * => The inverse operation should modify a text "xxyz" to "xyyz".
              */
             inverse.deltas.should.deep.equal([new Delta(0, "xx", "x"), new Delta(2, "y", "yy"), new Delta(3, "z", "z")])
+        })
+    })
+
+    describe('#rebase', () => {
+        it('return the same diff it the base is empty diff', () => {
+            const target = new Diff([new Delta(1, "s", "ss"), new Delta(4, "tt", "t")])
+            target.rebase(new Diff([])).should.deep.equal(target)
+        })
+        const base = new Diff([new Delta(0, "x", "xx"), new Delta(2, "yy", "y"), new Delta(6, "zz", "z")])
+        it('adjust offsets using the base diff', () => {
+            const target = new Diff([new Delta(1, "s", "ss"), new Delta(4, "tt", "t")])
+
+            /* Example,
+             * - the target text: "xsyyttzz"
+             * - base is applied: "xxsyttz"
+             * - base and target are applied: "xxssytz"
+             *
+             * => `target.rebase(base)` should modify a text "xxsyttz" to "xxssytz"
+             */
+            const rebased = target.rebase(base)
+            if (rebased instanceof Diff) {
+                rebased.deltas.should.deep.equal([new Delta(2, "s", "ss"), new Delta(4, "tt", "t")])
+            } else {
+                fail("rebase should return a Diff object")
+            }
+        })
+        it('return conflict if the removed range is overlapped', () => {
+            /* A conflict is occurred because both base and target1 remove "x" */
+            const target1 = new Diff([new Delta(0, "x", "ss"), new Delta(4, "tt", "t")])
+            const rebased1 = target1.rebase(base)
+            if (rebased1 instanceof DeleteAlreadyDeletedText) {
+                rebased1.should.deep.equal(new DeleteAlreadyDeletedText(0, "x"))
+            } else {
+                fail("rebase should return conflict")
+            }
+
+             /* A conflict is occurred because both base and target2 remove "y" */
+            const target2 = new Diff([new Delta(1, "x", "ss"), new Delta(3, "yt", "t")])
+            const rebased2 = target2.rebase(base)
+            if (rebased2 instanceof DeleteAlreadyDeletedText) {
+                rebased2.should.deep.equal(new DeleteAlreadyDeletedText(3, "y"))
+            } else {
+                fail("rebase should return conflict")
+            }
+
+            /* A conflict is occurred because both base and target3 remove "z" */
+            const target3 = new Diff([new Delta(1, "x", "ss"), new Delta(5, "tz", "t")])
+            const rebased3 = target3.rebase(base)
+            if (rebased3 instanceof DeleteAlreadyDeletedText) {
+                rebased3.should.deep.equal(new DeleteAlreadyDeletedText(6, "z"))
+            } else {
+                fail("rebase should return conflict")
+            }
         })
     })
 })
