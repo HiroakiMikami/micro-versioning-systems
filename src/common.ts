1fca16e90f353f5258e594099ca07fc13b7362c6
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-05-05 05:55:37 +0000

    Implement Interval class

--- src/common.ts
+++ src/common.ts
@@ -25,4 +25,46 @@ abstract class ConstrainedData {
     static mode: ExecutionMode = ExecutionMode.Release
 }
 
-export { ExecutionMode, ConstrainedData }
+/** An interval [this.begin:this.end) */
+class Interval extends ConstrainedData {
+    public readonly end: number
+
+    /**
+     * @param begin
+     * @param length The length of this interval
+     */
+    constructor (public readonly begin: number, public readonly length: number) {
+        super()
+        this.end = this.begin + length
+    }
+
+    /**
+     * @returns the intersection of this and rhs (null if there is no overlap)
+     */
+    public intersect(rhs: Interval): Interval | null {
+        if (this.begin > rhs.begin) {
+            return rhs.intersect(this)
+        }
+
+        /* Pre-condition: this.begin <= rhs.begin */
+        if (this.begin != rhs.begin && this.end <= rhs.begin) {
+            return null
+        }
+        const begin = Math.max(this.begin, rhs.begin)
+        const end = Math.min(this.end, rhs.end)
+
+        return new Interval(begin, Math.max(0, end - begin))
+    }
+
+    public toString(): string {
+        return `[${this.begin}:${this.end})`
+    }
+    public validate(): string | null {
+        if (this.length < 0) {
+            return `the length is negative (${this.length})`
+        }
+        return null
+    }
+}
+
+export { ExecutionMode, ConstrainedData, Interval }
